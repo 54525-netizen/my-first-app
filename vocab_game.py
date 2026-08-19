a891548cb0cc95db65ad858775e4e63bccb15594
402-32-patitta <54525@yupparaj.ac.th>
2026-08-19 15:25:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -104,6 +104,7 @@ ans3 = st.text_input(
 ans4 = st.text_input(
     "ข้อ 4: Use the `k _ y` to unlock. 🔑",
     value=st.session_state.ans4_val,
+)
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
